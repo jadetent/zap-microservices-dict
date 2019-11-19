d3eec7cc3fb012f09a2305ad3ba7a3a9c91e5a24
gleysonzap <gleyson.oliveira@contazap.com.br>
2019-11-19 16:04:24 -0200

    pdf de comprovante com dados estaticos

--- src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
+++ src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
@@ -19,6 +19,7 @@ import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.net.MalformedURLException;
 
 
@@ -38,32 +39,34 @@ public class ComprovanteService {
         Image image = new Image(data);
         Paragraph p = new Paragraph().add(image);
         Table table = new Table(3);
-        Cell cell = new Cell(3, 1).add(p).setBorder(Border.NO_BORDER).setVerticalAlignment(VerticalAlignment.MIDDLE);
-        table.addCell(cell);
-        table.addCell(gerarCabecalho("Comprovante de Transferência"));
-        table.addCell(new Cell(1,2).setBorder(Border.NO_BORDER).add(new Paragraph("Data Transação: 19/11/2019 17:00")));
-        table.addCell(new Cell(1,3).setBorder(Border.NO_BORDER).add(new Paragraph("Autenticação: AX155AS65466AQSDAS854Q%")));
-        /*
-        //table.addCell(new Cell().add(new Paragraph("Cell 1.2")));
-        table.addCell(new Cell()
-                .add(new Paragraph("Data 2")));
-        table.addCell(new Cell()
-                .add(new Paragraph("Data 3"))
-                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
-                .setPadding(5));
-
-
-        // Closing the document
-         */
+        Cell logo = new Cell(3, 1).add(p).setBorder(Border.NO_BORDER).setVerticalAlignment(VerticalAlignment.MIDDLE);
+        table.addCell(logo);
+        table.addCell(criarCelula(1,2,"Comprovante de Transferência",12).setBold());
+        table.addCell(criarCelula(1,2,"Data Transação: 19/11/2019 17:00"));
+        table.addCell(criarCelula(1,2,"Autenticação: AX155AS65466AQSDAS854Q%"));
         document.add(table);
+
+        document.add(detalheTransacao("Conta Origem - Telefone","1234566"));
+        document.add(detalheTransacao("Conta Destino - Telefone","98785646"));
+        document.add(detalheTransacao("Valor","R$ 122,33"));
         document.close();
 
         System.out.println("Image added");
     }
-    private Cell gerarCabecalho(String cabecalho){
-        Cell cell = new Cell(1, 2)
-        .add(new Paragraph(cabecalho))
-        .setBorder(Border.NO_BORDER);
-        return cell;
+    private Paragraph detalheTransacao(String campo, String valor){
+        Paragraph p= new Paragraph();
+        p.setFontSize(8);
+        p.setFirstLineIndent(55);
+        p.add(campo + ":" + valor);
+        return p;
+    }
+    private Cell criarCelula(String text){
+        return criarCelula(1, 1,text,8);
+    }
+    private Cell criarCelula(int rowspan, int colspan,String text){
+        return criarCelula(rowspan, colspan,text,8);
+    }
+    private Cell criarCelula(int rowspan, int colspan,String text, int fontFize ){
+        return new Cell(rowspan,colspan).setBorder(Border.NO_BORDER).add(new Paragraph(text).setFontSize(fontFize));
     }
 }
