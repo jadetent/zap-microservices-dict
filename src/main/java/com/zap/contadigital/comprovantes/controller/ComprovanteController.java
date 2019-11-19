ddd9727542b4bf72eb142d0ff0ddaac41c4104ba
gleysonzap <gleyson.oliveira@contazap.com.br>
2019-11-19 14:43:32 -0200

    pdf de comprovante em forma de tabela e com imagem - logo

--- src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
+++ src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
@@ -1,12 +1,6 @@
 package com.zap.contadigital.comprovantes.controller;
 
-
-import com.zap.contadigital.auth.JwtSession;
-import com.zap.contadigital.model.auth.Role;
-import com.zap.contadigital.model.auth.Roles;
-import com.zap.contadigital.model.auth.Usuario;
-import com.zap.contadigital.repository.RoleRepository;
-import com.zap.contadigital.repository.UsuarioRepository;
+import com.zap.contadigital.comprovantes.service.ComprovanteService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,15 +8,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/comprovantes")
 public class ComprovanteController {
+    @Autowired
+    private ComprovanteService service;
     @GetMapping("/text")
     public @ResponseBody String getText() {
         return "Hello world";
@@ -40,6 +34,40 @@ public class ComprovanteController {
     public @ResponseBody byte[] getPdfWithMediaType() throws IOException {
         InputStream in = new FileInputStream(new File("/dev/comprovante.pdf"));
         return IOUtils.toByteArray(in);
+    }
+
+    @GetMapping(
+            value = "/estatico-pdf",
+            produces = MediaType.APPLICATION_PDF_VALUE
+    )
+    public @ResponseBody byte[] transferencia() throws Exception {
+        service.getLogo();
+        File file = new File("/home/comprovantes", "transferencia.pdf");
+        InputStream in= new FileInputStream(file);;
+        return IOUtils.toByteArray(in);
+        /*
+        File file = new File("home", "comprovante.pdf");
+        Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(file));
+            document.open();
+            // adicionando um parágrafo ao documento
+            document.add(new Paragraph("Gerando um PDF usando iText em Java"));
+            // adicionando um parágrafo com fonte diferente ao arquivo
+            document.add(new Paragraph("Adicionando outro paragrafo", FontFactory.getFont(FontFactory.COURIER, 12)));
+            document.close();
+            in = new FileInputStream(file);
+        } catch(DocumentException de) {
+            System.err.println(de.getMessage());
+        } catch(IOException ioe) {
+            System.err.println(ioe.getMessage());
+        } finally {
+            document.close();
+        }
+
+        */
+
     }
 
 }
--- src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
+++ src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
@@ -1,4 +1,69 @@
 package com.zap.contadigital.comprovantes.service;
 
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+
+
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+
+
+@Service
 public class ComprovanteService {
+    public void getLogo() throws Exception {
+        String dest = "/home/comprovantes/transferencia.pdf";
+        PdfWriter writer = new PdfWriter(dest);
+
+        PdfDocument pdf = new PdfDocument(writer);
+
+        Document document = new Document(pdf);
+
+        String imFile = "/home/comprovantes/logo.jpg";
+        ImageData data = ImageDataFactory.create(imFile);
+
+        Image image = new Image(data);
+        Paragraph p = new Paragraph().add(image);
+        Table table = new Table(3);
+        Cell cell = new Cell(3, 1).add(p).setBorder(Border.NO_BORDER).setVerticalAlignment(VerticalAlignment.MIDDLE);
+        table.addCell(cell);
+        table.addCell(gerarCabecalho("Comprovante de Transferência"));
+        table.addCell(new Cell(1,2).setBorder(Border.NO_BORDER).add(new Paragraph("Data Transação: 19/11/2019 17:00")));
+        table.addCell(new Cell(1,3).setBorder(Border.NO_BORDER).add(new Paragraph("Autenticação: AX155AS65466AQSDAS854Q%")));
+        /*
+        //table.addCell(new Cell().add(new Paragraph("Cell 1.2")));
+        table.addCell(new Cell()
+                .add(new Paragraph("Data 2")));
+        table.addCell(new Cell()
+                .add(new Paragraph("Data 3"))
+                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .setPadding(5));
+
+
+        // Closing the document
+         */
+        document.add(table);
+        document.close();
+
+        System.out.println("Image added");
+    }
+    private Cell gerarCabecalho(String cabecalho){
+        Cell cell = new Cell(1, 2)
+        .add(new Paragraph(cabecalho))
+        .setBorder(Border.NO_BORDER);
+        return cell;
+    }
 }
