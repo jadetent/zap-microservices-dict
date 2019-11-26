7f2cf7fd41791aa4791df25e9bd4a0dce55f475f
gleysonzap <gleyson.oliveira@contazap.com.br>
2019-11-26 12:59:17 -0300

    implementacao com itextpdf7

--- src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
+++ src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
@@ -1,16 +1,8 @@
 package com.zap.contadigital.comprovantes.controller;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.html.simpleparser.HTMLWorker;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
 import com.zap.contadigital.comprovantes.service.ComprovanteService;
-import com.zap.contadigital.comprovantes.service.ComprovanteServiceIText;
-import com.zap.contadigital.comprovantes.util.TemplateBuilder;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-
 import org.springframework.web.bind.annotation.*;
 import java.io.*;
 import java.util.HashMap;
@@ -20,121 +12,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/comprovantes")
 public class ComprovanteController {
-    File file = new File("/home/comprovantes", "transferencia.pdf");
-    @Autowired
-    private ComprovanteServiceIText serviceText;
     @Autowired
     private ComprovanteService service;
-    @GetMapping("/text")
-    public @ResponseBody String getText() {
-        return "Hello world";
-    }
-    @GetMapping(value = "/pdf")
-    public @ResponseBody byte[] getPdf() throws IOException {
-        InputStream in = new FileInputStream(file);
-        return IOUtils.toByteArray(in);
-    }
     @GetMapping(
-            value = "/pdf-media",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] getPdfWithMediaType() throws IOException {
-        InputStream in = new FileInputStream(file);
-        return IOUtils.toByteArray(in);
-    }
-
-    @GetMapping(
-            value = "/other",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] other() throws Exception {
-        String comprovante = serviceText.gerarComprovanteOutroFavorecido();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/same",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] same() throws Exception {
-        String comprovante = serviceText.gerarComprovanteMesmoFavorecido();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/p2p",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] p2p() throws Exception {
-        String comprovante = serviceText.gerarComprovanteP2P();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/pagamento",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] pagamento() throws Exception {
-        String comprovante = serviceText.gerarComprovantePagamento();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/recarga/bilhete",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] recargaBilhete() throws Exception {
-        String comprovante = serviceText.gerarComprovanteRecargaBilheteUnico();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/recarga/celular",
+            value = "/recarga-celular",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
     public @ResponseBody byte[] recargaCelular() throws Exception {
-        String comprovante = serviceText.gerarComprovanteRecargaCelular();
-        InputStream in= new FileInputStream(new File(comprovante));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/html",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] html() throws Exception {
-        TemplateBuilder templateBuilder = new TemplateBuilder();
-        templateBuilder.setParametro("titulo", "CONTA ZAP");
-        templateBuilder.setParametro("setor", "RECIBO");
-        templateBuilder.setParametro("assunto", "Comprovante");
-        templateBuilder.setParametro("mensagem", "Recibo de Pagamentos");
-        String conteudo=templateBuilder.getConteudo();
-        String html = "/home/comprovantes/comprovante.html";
-        String pdf = "/home/comprovantes/comprovante.pdf";
-        Writer fileWriter = new FileWriter(html);
-        fileWriter.write(conteudo);
-        fileWriter.close();
-
-        Document document = new Document();
-        PdfWriter writer = PdfWriter.getInstance(document,
-                new FileOutputStream(pdf));
-        document.open();
-        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
-                new FileInputStream(html));
-        document.close();
-        InputStream in= new FileInputStream(new File(pdf));;
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(
-            value = "/p2p-template",
-            produces = MediaType.APPLICATION_PDF_VALUE
-    )
-    public @ResponseBody byte[] p2pTemplate() throws Exception {
         Map<String, String> parametros = new HashMap<String,String>();
-        parametros.put("protocolo","12331197098090910990");
-        parametros.put("cliente","LUCIA ALVES PEREIRA");
+        parametros.put("protocolo","F191C348-4D3D-BD5B-6B09-9DC3C9128D24");
+        parametros.put("cliente","LUCIA ALVES PEREIRA SILVA");
         parametros.put("cpfCnpj","123.456.789-10");
         parametros.put("operadora","TIM - SP");
-        String comprovante=service.comprovanteTexto("COMPROVANTE_P2P", parametros);
+        parametros.put("telefone","(11) 99801-1234");
+        parametros.put("documento","12345678");
+        parametros.put("data","22/11/19");
+        parametros.put("valor","R$ 20,00");
+
+        String comprovante=service.comprovante("COMPROVANTE_P2P", parametros);
         InputStream in= new FileInputStream(new File(comprovante));
         byte[] bytes=IOUtils.toByteArray(in);
         in.close();
--- src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
+++ src/main/java/com/zap/contadigital/comprovantes/service/ComprovanteService.java
@@ -1,13 +1,8 @@
 package com.zap.contadigital.comprovantes.service;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.html.simpleparser.HTMLWorker;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.html2pdf.HtmlConverter;
 import com.zap.contadigital.comprovantes.util.TemplateBuilder;
 import com.zap.contadigital.repository.ConfiguracaoRepository;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.io.*;
@@ -27,48 +22,16 @@ public class ComprovanteService {
         }
 
         String conteudo=templateBuilder.getConteudo(template);
-        String html = "/home/comprovantes/comprovante.html";
         String pdf = "/home/comprovantes/comprovante.pdf";
-        Writer fileWriter = new FileWriter(html);
-        fileWriter.write(conteudo);
-        fileWriter.close();
-
-        Document document = new Document();
-        PdfWriter writer = PdfWriter.getInstance(document,new FileOutputStream(pdf));
-        document.open();
-        XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream(html));
-        document.close();
+        System.getenv("ITEXT7_LICENSEKEY" + "/itextkey-html2pdf_typography.xml");
+        try {
+            createPdf(conteudo.toString(), pdf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return pdf;
     }
-    public String comprovanteTexto(String nomeChave, Map<String, String> parametros)  throws Exception {
-        String template = configuracaoRepository.findOne("COMPROVANTE_P2P",GRUPO).getValor();
-        TemplateBuilder templateBuilder = new TemplateBuilder();
-
-        for (Map.Entry<String, String> entry : parametros.entrySet()) {
-            templateBuilder.setParametro(entry.getKey(), entry.getValue());
-        }
-        String conteudo=templateBuilder.getConteudo(template);
-        String pdf = "/home/comprovantes/comprovante.pdf";
-         /*
-        Document document = new Document(PageSize.LETTER);
-        PdfWriter.getInstance(document, new FileOutputStream(pdf));
-        document.open();
-        //document.addAuthor("author");
-        //document.addSubject("subject");
-        //document.addCreationDate();
-        //document.addTitle("title");
-        HTMLWorker htmlWorker = new HTMLWorker(document);
-        htmlWorker.parse(new StringReader(conteudo));
-        document.close();
-         */
-        OutputStream file = new FileOutputStream(new File(pdf));
-        Document document = new Document();
-        PdfWriter writer = PdfWriter.getInstance(document, file);
-        document.open();
-        InputStream is = new ByteArrayInputStream(conteudo.getBytes());
-        XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
-        document.close();
-        file.close();
-        return pdf;
+    private void createPdf(String html, String dest) throws IOException {
+        HtmlConverter.convertToPdf(html, new FileOutputStream(dest));
     }
 }
