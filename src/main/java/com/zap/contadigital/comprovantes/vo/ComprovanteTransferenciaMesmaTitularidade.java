5abd895aa7b61dd3fd202907dee2efe476221d77
Gleyson <gleyson.oliveira@contazap.com.br>
2019-11-28 09:59:30 -0300

    criando classe Vo dos comprovantes

---
+++ src/main/java/com/zap/contadigital/comprovantes/vo/ComprovanteTransferenciaMesmaTitularidade.java
@@ -0,0 +1,85 @@
+package com.zap.contadigital.comprovantes.vo;
+
+public class ComprovanteTransferenciaMesmaTitularidade {
+    private String protocolo;
+    private String cliente;
+    private String cpfCnpj;
+    private String favorecido;
+    private String banco;
+    private String agencia;
+    private String cpfCnpjFavorecido;
+    private String valor;
+    private String data;
+
+    public String getProtocolo() {
+        return protocolo;
+    }
+
+    public void setProtocolo(String protocolo) {
+        this.protocolo = protocolo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getFavorecido() {
+        return favorecido;
+    }
+
+    public void setFavorecido(String favorecido) {
+        this.favorecido = favorecido;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getCpfCnpjFavorecido() {
+        return cpfCnpjFavorecido;
+    }
+
+    public void setCpfCnpjFavorecido(String cpfCnpjFavorecido) {
+        this.cpfCnpjFavorecido = cpfCnpjFavorecido;
+    }
+}
---
+++ src/main/java/com/zap/contadigital/comprovantes/vo/ComprovanteTransferenciaP2P.java
@@ -0,0 +1,58 @@
+package com.zap.contadigital.comprovantes.vo;
+
+public class ComprovanteTransferenciaP2P {
+    private String protocolo;
+    private String cliente;
+    private String cpfCnpj;
+    private String telefone;
+    private String valor;
+    private String data;
+
+    public String getProtocolo() {
+        return protocolo;
+    }
+
+    public void setProtocolo(String protocolo) {
+        this.protocolo = protocolo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
