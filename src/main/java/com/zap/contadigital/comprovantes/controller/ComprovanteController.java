185dc9b6985e7f32f652d44d586d1f8cdf1513da
Gleyson <gleyson.oliveira@contazap.com.br>
2019-12-02 11:26:05 -0300

    passando idTransacao como parametro nas requisicoes de comprovantes

--- src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
+++ src/main/java/com/zap/contadigital/comprovantes/controller/ComprovanteController.java
@@ -16,56 +16,62 @@ public class ComprovanteController {
     private ComprovanteService service;
 
     @GetMapping(
-            value = "/recarga-celular",
+            value = "/recarga-celular/{idTransacao}",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
-    public @ResponseBody byte[] comprovanteRecargaCelular() throws Exception {
-        ComprovanteRecargaCelularVo comprovante = new ComprovanteRecargaCelularVo();
-        comprovante.setIdTransacao("A7EE72D0-ECFD-7AB3-791A-FDB7CC01AE9C");
 
+    public @ResponseBody byte[] comprovanteRecargaCelular(@PathVariable("idTransacao") String idTransacao) throws Exception {
+        ComprovanteRecargaCelularVo comprovante = new ComprovanteRecargaCelularVo();
+        comprovante.setIdTransacao(idTransacao);
         byte[] bytes=service.gerarComprovanteRecargaCelular(comprovante);
         return bytes;
     }
 
     @GetMapping(
-            value = "/pagamento",
+            value = "/pagamento/{idTransacao}",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
-    public @ResponseBody byte[] comprovantePagamento() throws Exception {
+
+    public @ResponseBody byte[] comprovantePagamento(@PathVariable("idTransacao") String idTransacao) throws Exception {
         ComprovantePagamentoVo comprovante = new ComprovantePagamentoVo();
-        comprovante.setIdTransacao("A7EE72D0-ECFD-7AB3-791A-FDB7CC01AE9C");
+        comprovante.setIdTransacao(idTransacao);
         comprovante.setProtocolo("12348564");
         byte[] bytes=service.gerarComprovantePagamento(comprovante);
         return bytes;
     }
+
     @GetMapping(
-            value = "/transferencia-p2p",
+            value = "/transferencia-p2p/{idTransacao}",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
-    public @ResponseBody byte[] comprovanteTransferenciaP2p() throws Exception {
+
+    public @ResponseBody byte[] comprovanteTransferenciaP2p(@PathVariable("idTransacao") String idTransacao) throws Exception {
         ComprovanteTransferenciaP2PVo comprovante = new ComprovanteTransferenciaP2PVo();
-        comprovante.setProtocolo("A7EE72D0-ECFD-7AB3-791A-FDB7CC01AE9C");
+        comprovante.setProtocolo(idTransacao);
         byte[] bytes=service.gerarComprovanteTransferenciaP2p(comprovante);
         return bytes;
     }
+
     @GetMapping(
-            value = "/transferencia-mesma-titularidade",
+            value = "/transferencia-mesma-titularidade/{idTransacao}",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
-    public @ResponseBody byte[] comprovanteTransferenciaMesmaTitularidade() throws Exception {
+
+    public @ResponseBody byte[] comprovanteTransferenciaMesmaTitularidade(@PathVariable("idTransacao") String idTransacao) throws Exception {
         ComprovanteTransferenciaMesmaTitularidadeVo comprovante = new ComprovanteTransferenciaMesmaTitularidadeVo();
-        comprovante.setProtocolo("A7EE72D0-ECFD-7AB3-791A-FDB7CC01AE9C");
+        comprovante.setProtocolo(idTransacao);
         byte[] bytes=service.gerarComprovanteTransferenciaMesmaTitularidade(comprovante);
         return bytes;
     }
 
     @GetMapping(
-            value = "/transferencia-outra-titularidade",
+            value = "/transferencia-outra-titularidade/{idTransacao}",
             produces = MediaType.APPLICATION_PDF_VALUE
     )
-    public @ResponseBody byte[] comprovanteTransferenciaOutraTitularidade() throws Exception {
+
+    public @ResponseBody byte[] comprovanteTransferenciaOutraTitularidade(@PathVariable("idTransacao") String idTransacao) throws Exception {
         ComprovanteTransferenciaOutraTitularidadeVo comprovante = new ComprovanteTransferenciaOutraTitularidadeVo();
-        comprovante.setProtocolo("A7EE72D0-ECFD-7AB3-791A-FDB7CC01AE9C");
+        comprovante.setProtocolo(idTransacao);
         byte[] bytes=service.gerarComprovanteTransferenciaOutraTitularidade(comprovante);
         return bytes;
     }
