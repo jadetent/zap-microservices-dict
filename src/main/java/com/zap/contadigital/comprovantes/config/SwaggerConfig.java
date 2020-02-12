45d9bf7dee878de7e150315cd2f54367c5997379
Felipe F. Dicezare <felipe.dicezare@contazap.com.br>
2020-02-12 18:47:34 -0300

    Ajustes

--- src/main/java/com/zap/contadigital/comprovantes/config/SwaggerConfig.java
+++ src/main/java/com/zap/contadigital/comprovantes/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
-package com.zap.contadigital.doc;
+package com.zap.contadigital.comprovantes.config;
 
+import com.zap.contadigital.doc.BaseSwaggerConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Profile("!prod")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends BaseSwaggerConfig {
+
     public SwaggerConfig() {
         super("com.zap.contadigital.comprovantes.controller");
     }
